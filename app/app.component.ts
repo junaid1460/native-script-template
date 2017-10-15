39824f53fe44ed98f75ca6aad6ba788085ecbce1
junaid <junaid1460@gmail.com>
2017-10-15 21:16:53 +0530

    for interpolation/ text updates in label use text = "{{ var }}"

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,21 +5,23 @@ import { Component } from "@angular/core";
   template: `
     <ActionBar title="Hello" class="action-bar"></ActionBar>
     <StackLayout>
-    <TextField hint="type something" KeyboardType="email" height="50px"></TextField>
+    <TextField hint="type something" KeyboardType="email" height="50px" [(ngModel)]="email"></TextField>
     <Button text="helloword" height="50px" (tap)="log()"></Button>
+    <Label text="{{  email }}"></Label>
     </StackLayout>
   `
   ,
   styles: [`
     StackLayout{
-      padding:10px
+      padding:10px;
     }
   `]
 })
 export class AppComponent {
   // Your TypeScript logic goes here
+  email:any
   log(){
-    alert("It worked");
+    alert(this.email);
   }
 
 }
